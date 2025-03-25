75801d25c608e064da56ed3b24fa5eacf018f663
ethansmith2000 <ethansmith200@gmail.com>
2025-03-25 14:01:08 +0000

    base updates

--- relative_adam.py
+++ relative_adam.py
@@ -4,7 +4,7 @@ import torch.distributed as dist
 
 
 class RelativeAdam(torch.optim.Optimizer):
-    def __init__(self, params, lr=1e-4, weight_decay=0, beta1=0.9, beta2=0.999, eps=1e-8, lr_weight=0.5, param_lr=0.005, param_eps=1e-4, lr_cap=0.01):
+    def __init__(self, params, lr=1e-4, weight_decay=0, beta1=0.9, beta2=0.999, eps=1e-8, lr_weight=0.4, param_lr=0.01, param_eps=1e-4, lr_cap=0.01):
         defaults = dict(lr=lr, weight_decay=weight_decay, beta1=beta1, beta2=beta2, eps=eps, lr_weight=lr_weight, param_lr=param_lr, param_eps=param_eps, lr_cap=lr_cap)
         super().__init__(params, defaults)
 
